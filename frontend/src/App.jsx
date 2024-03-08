c3906c92dc8b8fa49353f7ae6d8a611bd7056761
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-09 00:49:13 +0200

    fixes to routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
@@ -23,11 +23,11 @@ function App() {
     <UserContext.Provider value={{loggedUser,setLoggedUser}}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={loggedUser!==null?<Homes />:<Home />}></Route>
-          <Route path='/login' element={loggedUser!==null?<Homes />:<Login />}></Route>
-          <Route path='/register' element={loggedUser!==null?<Homes />:<Register />}></Route>
+          <Route path='/' element={loggedUser!==null?<Navigate to="/homes" />:<Home />}></Route>
+          <Route path='/login' element={loggedUser!==null?<Navigate to="/homes" />:<Login />}></Route>
+          <Route path='/register' element={loggedUser!==null?<Navigate to="/homes" />:<Register />}></Route>
           <Route path='/homes' element={<Homes />}></Route>
-          <Route path='/profile' element={loggedUser!==null?<Profile />:<Home />}></Route>
+          <Route path='/profile' element={loggedUser!==null?<Profile />:<Navigate to="/" />}></Route>
           {/* <Route path='/profile' element={<Private Component={Profile} />}></Route> */}
           <Route path='*' element={<Notfound />}></Route>
         </Routes>
