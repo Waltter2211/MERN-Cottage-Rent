b22a9385e7eb389cb159ab69602e8a157cbf7cc7
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-03-15 16:45:48 +0200

    fixed bugs and optimized code on search functionality also commented pagination

--- frontend/src/components/Homes.jsx
+++ frontend/src/components/Homes.jsx
@@ -1,12 +1,14 @@
 import { useContext, useEffect, useState } from 'react'
 import Header from './Header'
 import HouseContext from '../contexts/HouseContext'
+import HomeSingle from './HomeSingle'
 import Paginate from './Paginate'
 
 function Homes() {
   
   const [houses, setHouses] = useState([])
   const [initialHouses, setInitialHouses] = useState([])
+  const [tag, setTag] = useState("")
   let containedHouses = useContext(HouseContext)
   let homeSearch
 
@@ -33,71 +35,20 @@ function Homes() {
   }, [])
 
   function handleTagSelect(event) {
-    /* console.log(event.target.id) */
-    /* if (tag === false) {
-      let tagHouses = houses.filter((house) => {
-        return house.houseTags === event.target.id
-      })
-      setHouses(tagHouses)
-      setTag(true)
-    }
-    else if (tag === true) {
-      setHouses(initialHouses)
-      setTag(false)
-    } */
     let houseList = initialHouses
-    switch(event.target.id) {
-      case "Tower Blocks":
-        let taggedHouseBlocks = houseList.filter((house) => {
-          return house.houseTags === "Tower Blocks"
+
+    setHouses(() => {
+      return (
+        houseList.filter((house) => {
+          return house.houseTags === event.target.id
         })
-        setHouses(taggedHouseBlocks)
-        break;
-      case "Cottages":
-        let taggedHouseCottages = houseList.filter((house) => {
-          return house.houseTags === "Cottages"
-        })
-        setHouses(taggedHouseCottages)
-        break;
-      case "Vans":
-        let taggedHouseVans = houseList.filter((house) => {
-          return house.houseTags === "Vans"
-        })
-        setHouses(taggedHouseVans)
-        break;
-      case "Others":
-        let taggedHouseOthers = houseList.filter((house) => {
-          return house.houseTags === "Others"
-        })
-        setHouses(taggedHouseOthers)
-        break;
-      default:
-        return initialHouses
-    }
+      )
+    })
+    setTag(event.target.id)
   }
   
   function handleSearch(event) {
     homeSearch = event.target.value
-    /* console.log(homeSearch, searchedHouses) */
-
-    /* fetch(`http://localhost:8000/houses/${homeSearch}`, {
-      method:"GET",
-      headers:{
-        "Authorization":localStorage.getItem("jsontoken")
-      }
-    })
-    .then((res) => {
-      console.log(res)
-      return res.json()
-    })
-    .then((data) => {
-      console.log(data)
-      setHouses(data.foundHouses)
-      containedHouses.setContainedHouses(houses)
-    })
-    .catch((err) => {
-      console.log(err)
-    }) */
 
     let searchHouses = houses.filter((house) => {
       const regex = new RegExp(`${event.target.value}`, 'i')
@@ -107,11 +58,19 @@ function Homes() {
     if (event.target.value === "") {
       setHouses(initialHouses)
       containedHouses.setContainedHouses(initialHouses)
+      clearTags()
     }
     else {
       setHouses(searchHouses)
     }
   }
+
+  function clearTags() {
+    setHouses(initialHouses)
+    setTag("")
+  }
+
+  /* console.log(houses) */
 
   return (
     <>
@@ -128,28 +87,28 @@ function Homes() {
           </div>
           <div className='homes-list'>
             <div className='homes-list-sidebar'>
-              <button onClick={() => {setHouses(initialHouses)}}>Test</button>
               <ul className='homes-list-buttons'>
-                <li className='homes-list-button' onClick={handleTagSelect} id='Cottages'>Cottages</li>
-                <li className='homes-list-button' onClick={handleTagSelect} id='Tower Blocks'>Tower Blocks</li>
-                <li className='homes-list-button' onClick={handleTagSelect} id='Vans'>Vans</li>
-                <li className='homes-list-button' onClick={handleTagSelect} id='Others'>Others</li>
+                {tag === "Cottages"?<li className='homes-list-button list-div' onClick={clearTags} id='Cottages'>Cottages</li>
+                :<li className='homes-list-button' onClick={handleTagSelect} id='Cottages'>Cottages</li>}
+                {tag === "Tower Blocks"?<li className='homes-list-button list-div' onClick={clearTags} id='Tower Blocks'>Tower Blocks</li>
+                :<li className='homes-list-button' onClick={handleTagSelect} id='Tower Blocks'>Tower Blocks</li>}
+                {tag === "Vans"?<li className='homes-list-button list-div' onClick={clearTags} id='Vans'>Vans</li>
+                :<li className='homes-list-button' onClick={handleTagSelect} id='Vans'>Vans</li>}
+                {tag === "Others"?<li className='homes-list-button list-div' onClick={clearTags} id='Others'>Others</li>
+                :<li className='homes-list-button' onClick={handleTagSelect} id='Others'>Others</li>}
               </ul>
             </div>
             <div className='homes-list-main'>
-              {/* <div className='homes-list-main-homes'>
-                {houses === undefined && homeSearch !== undefined? <h1>No Homes Found</h1>
-                :houses === undefined && homeSearch === undefined? houses?.map((house, index) => {
+              <div className='homes-list-main-homes'>
+                {houses.length === 0? <h1 className='blue'>No Homes Found</h1>
+                /* :houses === undefined && homeSearch === undefined? houses?.map((house, index) => {
                   return <HomeSingle key={index} house={house} />
-                })
+                }) */
                 :houses?.map((house, index) => {
                   return <HomeSingle key={index} house={house} />
                 })}
               </div>
-              <div className='homes-list-pagination'>
-                <Paginate houses={houses} itemsPerPage={15} />
-              </div> */}
-              <Paginate houses={houses} itemsPerPage={15} />
+              {/* <Paginate houses={houses} itemsPerPage={15} /> */}
             </div>
           </div>
         </div>
