134995e0d6d253d76a70df2e819aa0d263f20f42
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2024-05-22 14:06:24 +0300

    made script to that adds some sample houses

---
+++ backend/utils/addTestData.js
@@ -0,0 +1,19 @@
+const housesData = require('./testHouses.json')
+const housesModel = require('../models/housesModel')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const createHouses = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE || 'mongodb://localhost:27017')
+        housesData.forEach(async (house) => {
+            await housesModel.create(house)
+        })
+        const housesArr = await housesModel.find({})
+        console.log(`added ${housesArr.length} houses, you can now exit with ctrl+c`)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+createHouses()
