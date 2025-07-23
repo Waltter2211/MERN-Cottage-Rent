80731754630d3abc5d8de86196f7a6ea3864fe68
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2025-07-23 09:27:04 +0300

    updated home page login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import UserContext from "../contexts/UserContext";
 import SelectionContext from "../contexts/SelectionContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 
 function Login() {
   const navigate = useNavigate();
   let loggedUser = useContext(UserContext);
-  const { setSelection } = useContext(SelectionContext)
+  const { setSelection } = useContext(SelectionContext);
 
   let [message, setMessage] = useState({
     type: "",
@@ -77,37 +79,61 @@ function Login() {
   }
 
   return (
-    <>
+    <div className="login-frame">
       <div className="login">
-        <form className="form">
-          <p>Email</p>
-          <input
-            type="email"
-            placeholder="Email..."
-            className="inp"
-            onChange={handleInput}
-            name="email"
-            value={loggingUser.email}
-          ></input>
-          <p>Password</p>
-          <input
-            type="password"
-            placeholder="Password..."
-            className="inp"
-            onChange={handleInput}
-            name="password"
-            value={loggingUser.password}
-          ></input>
-          <button className="btn" onClick={handleLogin}>
-            Login
+        <div className="login-description">
+          <h2>Welcome Back!</h2>
+          <p>Sign in to access your rentals</p>
+        </div>
+        <form className="login-form">
+          <div className="login-form-input-frame">
+            <p>Email</p>
+            <div className="login-form-input">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <input
+                type="email"
+                placeholder="Email..."
+                onChange={handleInput}
+                name="email"
+                value={loggingUser.email}
+              ></input>
+            </div>
+          </div>
+          <div className="login-form-input-frame">
+            <p>Password</p>
+            <div className="login-form-input">
+              <FontAwesomeIcon icon={faLock} />
+              <input
+                type="password"
+                placeholder="Password..."
+                onChange={handleInput}
+                name="password"
+                value={loggingUser.password}
+              ></input>
+            </div>
+          </div>
+          <button className="button" onClick={handleLogin}>
+            LOGIN
           </button>
           <p className={message.type}>{message.text}</p>
         </form>
-        <p>
-          Dont have an account? <span onClick={() => setSelection(2)}>Register</span>
-        </p>
+        <div className="login-sub-description">
+          <h3>
+            Don&apos;t have an account?{" "}
+            <span
+              className="login-sub-description-highlight"
+              onClick={() => setSelection(2)}
+            >
+              Register
+            </span>
+          </h3>
+          <div className="login-line"></div>
+          <p className="login-terms-text">
+            By continuing, you agree to our Terms of Service and Privacy Policy
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
