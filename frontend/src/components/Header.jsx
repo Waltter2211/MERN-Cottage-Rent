89636340cbddb596d5c3d0759e6ecedc2245139e
Waltter2211 <110539577+Waltter2211@users.noreply.github.com>
2025-08-28 02:55:38 +0300

    started creating responsive design for single home info page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -225,8 +225,28 @@ function Header({
                       </div>
                     )}
                   {location.pathname.includes("/homeSingle") && (
-                    <div>
-                      <p>Home single</p>
+                    <div className="header-sidebar-mobile-home">
+                      <h1>MERN-Rent</h1>
+                      <div
+                        className="header-sidebar-mobile-home-button-frame"
+                        onClick={() => handleNavigation(1)}
+                      >
+                        <p>My Rentals</p>
+                      </div>
+                      <div
+                        className="header-sidebar-mobile-home-button-frame header-sidebar-mobile-home-button-frame-selected"
+                        onClick={() => handleNavigation(2)}
+                      >
+                        <p>Account</p>
+                      </div>
+                      <div
+                        className="header-sidebar-mobile-home-button-frame mobile-button-color-blue"
+                        onClick={logout}
+                      >
+                        <p>
+                          <FontAwesomeIcon icon={faArrowRightFromBracket} /> Logout
+                        </p>
+                      </div>
                     </div>
                   )}
                 </div>
